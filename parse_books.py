f66b1297ad8ad2f536390cb8033115a6d20e7a2c
Vitaly Borodulin <rainbowf@mail.ru>
2022-04-16 16:44:31 +0500

    Add parse_book function

--- parse_books.py
+++ parse_books.py
@@ -1,6 +1,7 @@
 import os
 import requests
 
+from bs4 import BeautifulSoup
 from urllib.error import HTTPError
 
 
@@ -17,6 +18,17 @@ def check_for_redirect(response: requests.models.Response) -> None:
             )
 
 
+def parse_book(id: int) -> tuple[str, str]:
+    '''Parse book title and author.'''
+    url = f'https://tululu.org/b{id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title = soup.find('h1').text
+    title = title.split('::')
+    return title[0].strip(), title[1].strip()
+
+
 def download_books(books_count: int) -> None:
     '''Download books from tululu.org.'''
     os.makedirs('books', exist_ok=True)
@@ -29,7 +41,8 @@ def download_books(books_count: int) -> None:
             check_for_redirect(response)
         except HTTPError:
             continue
-        filename = f'books/book_id{id}.txt'
+        book = parse_book(id)
+        filename = f'books/{book[1]} - {book[0]}.txt'
         with open(filename, 'w') as file:
             file.write(response.text)
 
